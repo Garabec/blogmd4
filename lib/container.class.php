545f1fb7c336a7c043816135b995e1544f9f0cf2
Timofei <dion26@yandex.ru>
2017-01-18 19:56:57 +0000

    cont_class repamanager

---
+++ lib/container.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class Container {
+    
+    
+ private $elements=array();
+ 
+ 
+ public function set($key,$value){
+     
+    $elements[$key]=$value; 
+     
+ }
+    
+    
+public function get($key) {
+    
+  if(isset($this->elements[$key])) {return  $this->elements[$key];}
+  
+     else {  throw new Exception("Not element container $key");};
+    
+    
+}   
+    
+    
+    
+}
---
+++ lib/repasitorymanadger.class.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class RepasitoryManager{
+    
+    
+ private $repasitories=array();
+ 
+ 
+ public function set($key,$value){
+     
+   $repasitories[$key]=$value;  
+     
+ }
+    
+ 
+ public function has($key){
+     
+     
+  return isset($repasitories[$key]) ;  
+     
+ }
+ 
+ 
+ public function get($key){
+     
+     
+     if($this->has($key)){
+         
+        return $repasitories[$key] ;
+         
+     }
+ }
+    
+    
+}
